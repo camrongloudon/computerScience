c8605c91230d270e1d9758883938dd3a4712d6a1
camron_glo <gloudoncamron9@gmail.com>
2023-10-02 14:04:29 -0400

    Add files via upload

--- stupidName.cpp
+++ stupidName.cpp
@@ -1,4 +1,5 @@
 /* Write a program to ask someone for their name tell them they have a stupid name */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -10,9 +11,15 @@ char name [25];
 printf("Enter your name: ");
 scanf("%s", name);
 printf("\n");
-
-printf("%s, is a stupid name !!!", name);
-printf("\n");
+	
+while (strcmp(name, "end") != 0) {
+	printf("%s, is a stupid name !!!", name);
+	printf("\n");
+	
+	printf("Enter your name: ");
+	scanf("%s", name);
+	printf("\n");
+}
 
 system("pause");
 }
