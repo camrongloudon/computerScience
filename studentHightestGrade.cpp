d5b7ee7cac377b116c0cb9fc54ebb1964b3b1034
camron_glo <gloudoncamron9@gmail.com>
2023-10-04 14:10:19 -0400

    Add files via upload

---
+++ studentHightestGrade.cpp
@@ -0,0 +1,5 @@
+/* Write a program to ask for a student's name and how many subjects they are doing.
+   Ask for the marks for each subject, calculate the average and output if the student passed or failed. 
+   Output the amount of students in the class, the amount passed, the amount failed after typing finished.
+   
+   Lastly, output the name of the student that got the highest average score.*/ 
