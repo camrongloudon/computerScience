56dc1848b73ec8128affa5bc10a3fc2b6ea9204f
camron_glo <gloudoncamron9@gmail.com>
2023-10-26 14:05:34 -0400

    Add files via upload

--- numArraySelectionSort.cpp
+++ numArraySelectionSort.cpp
@@ -8,7 +8,7 @@ void selectionsort(int arr[], int size) {
 	for (int i = 0; i < size - 1; i++) {
 		int smallest = i;
 		for (int y = i + 1; y < size; y++) {
-			if (arr[y] < arr[smallest])
+			if (arr[y] > arr[smallest])
 				smallest = y;
 		}
 		int temp = arr[smallest];
