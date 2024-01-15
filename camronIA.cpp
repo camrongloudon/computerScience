d1b2e1fa665d6fc61303bee5abd61931f436a500
camron_glo <gloudoncamron9@gmail.com>
2024-01-15 14:09:41 -0400

    Add files via upload
    implemented while loop and arrays to hold multiple competitors for the race. 
    *minor issue here* 
    the printing of some floating point number do not succeed. They print trailing zeros instead.

--- camronIA.cpp
+++ camronIA.cpp
@@ -2,17 +2,50 @@
 #include<stdlib.h>
 #include<string.h>
 
+void selectionSortLow(float arr[], char name[][20], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		int smallest = i;
+		for (int y = i + 1; y < size; y++) {
+			if (arr[y] < arr[smallest])
+				smallest = y;
+		}
+	int temp = arr[smallest];
+	arr[smallest] = arr[i];
+	arr[i] = temp;
+	
+	char tempName[25];
+        strcpy(tempName, name[smallest]);
+        strcpy(name[smallest], name[i]);
+        strcpy(name[i], tempName);
+	}
+}
 int main ( ) {
-	int position;
-	float timeTaken;
-	char name[20];
+	int counter;
+	float timeTaken[100];
+	char name[100][20];
 	
-	printf("Athlete Name: ");
-	scanf("%s", &name);
-	
-	printf("Athlete Time: ");
-	scanf("%f", &timeTaken);	
-	
-	printf("%s took %5.4f to finish the race!", name, timeTaken);
+	counter = 0;
 
+	printf("Athlete Name: ");
+	scanf("%s", &name[counter]);
+
+	while(strcmp(name[counter], "end") != 0) {  
+		printf("Athlete Time: ");
+		scanf("%f", &timeTaken[counter]);
+
+		counter++;	
+		
+		printf("Athlete Name: ");
+		scanf("%s", &name[counter]);
+	}	
+	selectionSortLow(timeTaken, name, counter);
+	
+	printf(" _______________________\n");
+	printf("| Athlete Name  | Time	|\n");
+	printf("|-------------------------------------------------------|\n");
+
+	for (int x = 0; x < counter; x++) {
+	printf(" %s            | %5.6f |\n", name[x], timeTaken[x]);
+	}
+	printf("|-------------------------------------------------------|\n");
 }
